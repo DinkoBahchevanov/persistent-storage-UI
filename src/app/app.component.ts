986055047b903e307ab865b75497c55600140fc9
unknown <dinko_bahchevanov@abv.bg>
2021-10-05 18:25:28 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Product } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'persistence-storage-app';
+
+export class AppComponent implements OnInit {
+  public products: Product[];
+
+  constructor(private productService: ProductService){ this.products = [];}
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  public getProducts(): void {
+    this.productService.getProducts().subscribe(
+      (response: Product[]) => {
+        this.products = response;
+        console.log(this.products);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteProduct(productId: string): void {
+    this.productService.deleteProduct(productId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getProducts();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
---
+++ src/app/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from './product';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiServerUrl}/api/products`);
+  }
+
+  public deleteProduct(productId: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/api/products/${productId}`);
+  }
+}
